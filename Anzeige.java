e3bdd631355017f1091628c78db51b69df4c61a7
Crouc <kaluza.michel@gmx.de>
2017-04-03 23:15:02 +0200

    Update Anzeige.java

--- Anzeige.java
+++ Anzeige.java
@@ -2,8 +2,6 @@ package kleinanzeigen;
 
 import java.util.Date;
 
-/*********************  Niklas Schulligen   Sascha Magard  ************************************/
-/*********************  Elena Schumacher    Marcel Matschiner   *******************************/
 
 public class Anzeige {
     // Attribute
